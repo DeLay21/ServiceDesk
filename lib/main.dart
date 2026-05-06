c08e01d9b4ff7b22ce9e3343602e8cd0e0c77c89
Isaac <zazaq32@gmail.com>
2026-05-06 17:52:58 -0300

    feat(main.dart): Construção inicial do main

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,14 @@ import 'login.dart';
 void main() {
   runApp(const ServiceDesk());
 }
+
+class ServiceDesk extends StatelessWidget {
+  const ServiceDesk({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Color.fromRGBO(27, 79, 138, 1)),
+    );
+  }
+}
