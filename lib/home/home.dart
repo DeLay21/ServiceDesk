d332ad1d7a0d7b4f23976c02b315fe51eb7be186
DeLay21 <90519502+DeLay21@users.noreply.github.com>
2026-05-13 11:20:10 -0300

    Estrutura de data e perfil na home

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -11,6 +11,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+    Widget _CaixaDias(String dia, String semana, bool selecionado) {
+        return Container(
+            padding: EdgeInsets.symmetric(horizontal: 28, vertical: 18),
+            decoration: BoxDecoration(
+                color: selecionado
+                    ? Color.fromRGBO(27, 79, 138, 1) : Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+                children: [
+                    Text(
+                        dia,
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: selecionado
+                                ? Colors.white : Colors.black,
+                        ),
+                    ),
+                    Text(
+                        semana,
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: selecionado
+                            ? Colors.white : Colors.black,
+                        ),
+                    )
+                ],
+            ),
+        );
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,7 +86,7 @@ class _HomePageState extends State<HomePage> {
                                 IconButton(
                                     onPressed: () {
                                         Navigator.push(
-                                            context, 
+                                            context,
                                             MaterialPageRoute(builder: (context) => ConfigPage()),
                                         );
                                     },
@@ -75,6 +108,129 @@ class _HomePageState extends State<HomePage> {
                             borderRadius: BorderRadius.circular(30),
                             borderSide: BorderSide.none,
                         ),
+                    ),
+                ),
+                SizedBox(height: 20,),
+
+                Container(
+                    width: double.infinity,
+                    height: 180,
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(232, 238, 247,1),
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                            Text(
+                                'April',
+                                style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromRGBO(27, 79, 138,1)
+                                ),
+                            ),
+                            SizedBox(height: 20,),
+
+                            Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                    _CaixaDias('9', 'SEG', false),
+                                    _CaixaDias('10', 'TER', false),
+                                    _CaixaDias('11', 'QUA', true),
+                                    _CaixaDias('12', 'QUI', false),
+                                    _CaixaDias('13', 'SEX', false),
+                                    _CaixaDias('14', 'SAB', false),
+                                ],
+                            ),
+                        ],
+                    ),
+                    
+                ),
+
+                SizedBox(height: 20,),
+
+                Container(
+                    padding: EdgeInsets.all(20),
+                    width: 890,
+                    height: 130,
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(232, 238, 247, 1),
+                        borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                            CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Color.fromRGBO(27, 79, 138, 1),
+                                child: Icon(Icons.person, color: Colors.white, size: 40,),
+                            ),
+                            
+                            SizedBox(width: 20,),
+
+                            Expanded(
+                                child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                        Text(
+                                            'Isaac',
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold,
+                                            ),
+                                        ),
+                                        Text(
+                                            'Furador de Bolo',
+                                            style: TextStyle(
+                                                fontSize: 16,
+                                            ),
+                                        ),
+                                        
+                                        Container(
+                                            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2,),
+                                            decoration: BoxDecoration(
+                                                color: Color.fromRGBO(27, 79, 138, 1),
+                                                borderRadius: BorderRadius.circular(10),
+                                            ),
+                                            child: Text(
+                                                '15:00',
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.white,
+                                                ),
+                                            ),
+                                        )
+                                    ],
+                                ),
+                            ),
+                            Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                    Container(
+                                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                                        decoration: BoxDecoration(
+                                            color: Color.fromRGBO(27, 79, 138, 1),
+                                            borderRadius: BorderRadius.circular(20),
+                                        ),
+                                        child: Text(
+                                            '15/04',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white,
+                                            ),
+                                        ),
+                                    ),
+                                    SizedBox(height: 15,),
+                                    Icon(
+                                        Icons.arrow_forward_ios,
+                                        size: 30,
+                                    )
+                                ]
+                            )
+                        ],
                     ),
                 )
             ],
